5e4ea55b104910818cbadfd9e77d0160194241d0
Dailun Cheng <dc924@cornell.edu>
2018-04-26 14:27:54 -0400

    front end tracing for user login request complete.

--- front-end/api/user/index_origin.js
+++ front-end/api/user/index_origin.js
@@ -3,31 +3,6 @@
 
     var async = require("async"), express = require("express"), request = require("request"), endpoints = require("../endpoints"), helpers = require("../../helpers"), app = express(), cookie_name = "logged_in"
 
- 
-    const CLSContext = require('zipkin-context-cls');
-    const {Tracer} = require('zipkin');
-    const {recorder} = require('./recorder');
-    const rest = require('rest');
-    const {restInterceptor} = require('zipkin-instrumentation-cujojs-rest');
-    const defaultRequest = require('rest/interceptor/defaultRequest');
-    const {expressMiddleware, wrapExpressHttpProxy} = require('zipkin-instrumentation-express')
-    const proxy = require('express-http-proxy');
-
-    const ctxImpl = new CLSContext();
-    const tracer = new Tracer({ctxImpl, recorder});
-
-/*
-// Allow cross-origin, traced requests. See http://enable-cors.org/server_expressjs.html
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', [
-    'Origin', 'Accept', 'X-Requested-With', 'X-B3-TraceId',
-    'X-B3-ParentSpanId', 'X-B3-SpanId', 'X-B3-Sampled'
-  ].join(', '));
-  next();
-});
-*/
-
 
     app.get("/customers/:id", function(req, res, next) {
         helpers.simpleHttpRequest(endpoints.customersUrl + "/" + req.session.customerId, res, next);
@@ -272,45 +247,18 @@ app.use((req, res, next) => {
         );
     });
 
-//app.use(expressMiddleware({tracer,serviceName:'frontend-user'}));
-
     app.get("/login", function(req, res, next) {
         console.log("Received login request");
 
-        var loginHeader = rest.wrap(defaultRequest, { headers: { 'Authorization': req.get('Authorization') } });
-        var zipkinRestWithLogin =  loginHeader.wrap(restInterceptor, {tracer, serviceName: 'frontend_login'});
-        var getHeader = rest.wrap(defaultRequest, { headers: {method: 'GET'} });
-        var zipkinRestWithGet =  getHeader.wrap(restInterceptor, {tracer, serviceName: 'frontend_login'});;
-
         async.waterfall([
                 function(callback) {
-			zipkinRestWithLogin(endpoints.loginUrl)
-			.then(
-				function(response) {
-					var body = response.entity;
-					if(body.error){
-                            			callback(error);
-                            			return;
-					}
-                        		if (response.status.code == 200 && body != null && body != "") {
-                            			console.log(body);
-                            			var customerId = JSON.parse(body).user.id;
-                            			console.log(customerId);
-                            			req.session.customerId = customerId;
-                            			callback(null, customerId);
-                            			return;
-                        		}
-                        		console.log(response.status.code);
-                        		callback(true);
-                     		}).catch(err => console.error('Error', err.stack))
-                   /*
                     var options = {
                         headers: {
                             'Authorization': req.get('Authorization')
                         },
                         uri: endpoints.loginUrl
                     };
-                    request(options, function(error, response, body) { 
+                    request(options, function(error, response, body) {
                         if (error) {
                             callback(error);
                             return;
@@ -325,28 +273,12 @@ app.use((req, res, next) => {
                         }
                         console.log(response.statusCode);
                         callback(true);
-                    });*/
+                    });
                 },
                 function(custId, callback) {
                     var sessionId = req.session.id;
                     console.log("Merging carts for customer id: " + custId + " and session id: " + sessionId);
-			var url=endpoints.cartsUrl + "/" + custId + "/merge" + "?sessionId=" + sessionId;
-			zipkinRestWithGet(url)
-                        .then(
-                                function(response) {
-                                        var body = response.entity;
-                                        if(body.error){
-                                                // if cart fails just log it, it prevenst login
-                            			console.log(error);
-                            			//return;
-                                        }
-                                        console.log('Carts merged.');
-                                        callback(null, custId);
 
-                                }).catch(err => console.error('Error', err.stack))
-				
-
-		    /*
                     var options = {
                         uri: endpoints.cartsUrl + "/" + custId + "/merge" + "?sessionId=" + sessionId,
                         method: 'GET'
@@ -359,7 +291,7 @@ app.use((req, res, next) => {
                         }
                         console.log('Carts merged.');
                         callback(null, custId);
-                    });*/
+                    });
                 }
             ],
             function(err, custId) {
@@ -376,7 +308,7 @@ app.use((req, res, next) => {
                 console.log("Sent cookies.");
                 res.end();
                 return;
-            }); 
+            });
     });
 
     module.exports = app;
