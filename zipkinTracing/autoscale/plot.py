1c9950b500c56597cc07d6cbed21b3f531ac06ff
prevosis <aadilzbhatti@gmail.com>
2018-05-22 22:49:17 -0400

    plotting fixed

--- zipkinTracing/autoscale/plot.py
+++ zipkinTracing/autoscale/plot.py
@@ -1,11 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-times = np.loadtxt('times')
 latencies = np.loadtxt('latencies')
+scale_times = np.loadtxt('scale_times').astype(int)
+scale_times2 = np.delete(scale_times, np.argwhere(scale_times >= latencies.shape[0]))
 
-plt.plot(times, latencies)
+plain_latencies, = plt.plot(latencies[:, 0], latencies[:, 1])
+scale_time_latencies, = plt.plot(scale_times2, latencies[:, 1][scale_times2], 'ro')
 plt.xlabel('Time (s)')
 plt.ylabel('Latency (ms)')
-plt.title('End-to-End latency of orders with 10,000 requests, 2 clients')
+#plt.title('End-to-End latency of orders with 10,000 requests, 2 clients')
+plt.legend([plain_latencies, scale_time_latencies], ['Latencies', 'Scale Up Events'])
 plt.show()
